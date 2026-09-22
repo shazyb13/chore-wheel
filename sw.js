742cd6fd2f9890944d7db04afd54f385c80b6abb
shazyb13 <140645444+shazyb13@users.noreply.github.com>
2026-09-22 19:44:16 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this version whenever you re-upload index.html so phones pick up the new
 // version instead of serving the old cached one.
-const CACHE = 'chore-wheel-v4';
+const CACHE = 'chore-wheel-v5';
 
 const SHELL = [
   './',
